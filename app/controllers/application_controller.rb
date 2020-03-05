602ca0a652108f7654c0bf7fd1fe4059642b8eda
Alex Lee <hyunho98@gmail.com>
2020-03-05 22:02:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,14 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+
 end
